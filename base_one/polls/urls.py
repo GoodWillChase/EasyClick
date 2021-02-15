47c9c58c452d675bbdbef1f72e56e4ad846d68f9
GoodWillChase <2275352589@qq.com>
2021-02-15 16:57:15 +0800

    初步实现投票功能

--- base_one/polls/urls.py
+++ base_one/polls/urls.py
@@ -2,4 +2,17 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('', views.index, name='index')]
+# 指定该应用的命名空间
+app_name = 'polls'
+
+urlpatterns = [path('', views.index, name='index'),
+
+               # 例如: /polls/5/
+               path('<int:question_id>/', views.detail, name='detail'),
+
+               # 例如: /polls/5/results/
+               path('<int:question_id>/results/', views.results, name='results'),
+
+               # 例如: /polls/5/vote/
+               path('<int:question_id>/vote/', views.vote, name='vote'),
+               ]
